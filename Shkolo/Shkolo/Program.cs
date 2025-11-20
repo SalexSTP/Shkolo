6fe4fb2d7dc58bc7bbaed6d10d7b6c8a8900eeb3
IvailoKirilov <ivaylo.kirilov.highschool@buditel.bg>
2025-11-20 13:28:22 +0200

    Implemented the AddGrade, AddPraise and AddRemark Methods

--- Shkolo/Shkolo/Program.cs
+++ Shkolo/Shkolo/Program.cs
@@ -1,7 +1,4 @@
-<<<<<<< Updated upstream
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-=======
+
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,8 +6,7 @@ using System.Linq;
 using System.Text.Json;
 using Shkolo.Models;
 using Shkolo.Enums;
-
-
+using System.Diagnostics;
 
 var options = new JsonSerializerOptions
 {
@@ -18,6 +14,8 @@ var options = new JsonSerializerOptions
 };
 
 List<Student> students = LoadData();
+
+string json = File.ReadAllText("data.json");
 
 // simple menu loop
 while (true)
@@ -42,7 +40,7 @@ while (true)
         case "4": SearchStudent(); break;
         case "5": DeleteStudent(); break;
         case "6":
-            SaveData(students); // Pass the 'students' list to the SaveData method.
+            SaveData(students);
             Console.WriteLine("Saved. Goodbye!");
             return;
         case "7":
@@ -56,16 +54,281 @@ while (true)
 
 void PrintInfo(Student s)
 {
+
 }
 
 void AddGrade(Student s)
 {
+    Console.Write("Enter grade value(2-6):");
+    if(!double.TryParse(Console.ReadLine(), out double value))
+    {
+        Console.WriteLine("invalid grade");
+        return;
+    }
+    Console.WriteLine("\nChoose Grade Reason:");
+    int index1 = 0;
+    
+    foreach (var name in Enum.GetNames(typeof(GradeReason)))
+    {
+        Console.WriteLine($"{index1} + {name}");
+        index1++;
+    }
+
+    Console.Write("choose");
+    string input = Console.ReadLine()!;
+
+    GradeReason reason = new GradeReason();
+
+    switch (input) 
+    {
+        case "1": reason = GradeReason.Exam; break;
+
+        case "2": reason = GradeReason.ActiveParticipation; break;
+
+        case "3": reason = GradeReason.Homework; break;
+        
+        case "4": reason = GradeReason.Project; break;
+
+        case "5": reason = GradeReason.OralExamination; break;
+
+        case "6": reason = GradeReason.EntranceExam; break;
+
+        case "7": reason = GradeReason.TermExam; break;
+
+        case "8": reason = GradeReason.FinalExam; break;
+
+        case "9": reason = GradeReason.Other; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+
+    int index2 = 0;
+
+    foreach (var subjectName in Enum.GetNames(typeof(Subject)))
+    {
+        Console.WriteLine($"{index2} + {subjectName}");
+        index2++;
+    }
+
+    Console.Write("Choose subject: ");
+    string subjectInput = Console.ReadLine()!;
+
+    Subject subject = new Subject();
+
+    switch (subjectInput)
+    {
+        case "1": subject = Subject.Bulgarian; break;
+
+        case "2": subject = Subject.Mathematics; break;
+
+        case "3": subject = Subject.Physics; break;
+
+        case "4": subject = Subject.Chemistry; break;
+
+        case "5": subject = Subject.Biology; break;
+
+        case "6": subject = Subject.History; break;
+
+        case "7": subject = Subject.Geography; break;
+
+        case "8": subject = Subject.English; break;
+
+        case "9": subject = Subject.Programming; break;
+
+        case "10": subject = Subject.Business; break;
+
+        case "11": subject = Subject.Design; break;
+
+        case "12": subject = Subject.Russian; break;
+
+        case "13": subject = Subject.PhysicalEducation; break;
+
+        case "14": subject = Subject.German; break;
+
+        case "15": subject = Subject.Spanish; break;
+
+        case "16": subject = Subject.Philosophy; break;
+
+        case "17": subject = Subject.Informatics; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+
+    Console.Write("Enter teacher name: ");
+    Teacher teacher = new Teacher(Console.ReadLine()!, Console.ReadLine()!);
+
+    Grade grade = new Grade(subject, value, reason, teacher);
+
+    s.AddGrade(grade);
+
+    File.WriteAllText("data.json", JsonSerializer.Serialize(grade, options));
 }
 void AddPraise(Student s)
 {
+    Console.Write("Choose praise reason: ");
+
+    string praiseSubject = Console.ReadLine()!;
+
+    Subject subject = new Subject();
+
+    switch (praiseSubject)
+    {
+        case "1": subject = Subject.Bulgarian; break;
+
+        case "2": subject = Subject.Mathematics; break;
+
+        case "3": subject = Subject.Physics; break;
+
+        case "4": subject = Subject.Chemistry; break;
+
+        case "5": subject = Subject.Biology; break;
+
+        case "6": subject = Subject.History; break;
+
+        case "7": subject = Subject.Geography; break;
+
+        case "8": subject = Subject.English; break;
+
+        case "9": subject = Subject.Programming; break;
+
+        case "10": subject = Subject.Business; break;
+
+        case "11": subject = Subject.Design; break;
+
+        case "12": subject = Subject.Russian; break;
+
+        case "13": subject = Subject.PhysicalEducation; break;
+
+        case "14": subject = Subject.German; break;
+
+        case "15": subject = Subject.Spanish; break;
+
+        case "16": subject = Subject.Philosophy; break;
+
+        case "17": subject = Subject.Informatics; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+
+    Console.Write("Enter praise type: ");
+    string praiseType = Console.ReadLine()!;
+
+    PraiseType prType = new PraiseType();
+
+    switch (praiseType)
+    {
+        case "1": prType = PraiseType.ActiveParticipation; break;
+
+        case "2": prType = PraiseType.ExcellentPerformance; break;
+
+        case "3": prType = PraiseType.Improvement; break;
+
+        case "4": prType = PraiseType.Teamwork; break;
+
+        case "5": prType = PraiseType.Leadership; break;
+
+        case "6": prType = PraiseType.Creativity; break;
+
+        case "7": prType = PraiseType.Consistency; break;
+
+        case "8": prType = PraiseType.Responsibility; break;
+
+        case "9": prType = PraiseType.TaskCompletion; break;
+
+        case "10": prType = PraiseType.Nap; break;
+
+        case "11": prType = PraiseType.Other; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+    Praise praise = new Praise(subject, prType);
+
+    s.AddPraise(praise);
+
+    File.WriteAllText("data.json", JsonSerializer.Serialize(praise, options));
 }
 void AddRemark(Student s)
 {
+    Console.Write("Choose remark reason: ");
+
+    string remarkSubject = Console.ReadLine()!;
+
+    Subject subject = new Subject();
+
+    switch (remarkSubject)
+    {
+        case "1": subject = Subject.Bulgarian; break;
+
+        case "2": subject = Subject.Mathematics; break;
+
+        case "3": subject = Subject.Physics; break;
+
+        case "4": subject = Subject.Chemistry; break;
+
+        case "5": subject = Subject.Biology; break;
+
+        case "6": subject = Subject.History; break;
+
+        case "7": subject = Subject.Geography; break;
+
+        case "8": subject = Subject.English; break;
+
+        case "9": subject = Subject.Programming; break;
+
+        case "10": subject = Subject.Business; break;
+
+        case "11": subject = Subject.Design; break;
+
+        case "12": subject = Subject.Russian; break;
+
+        case "13": subject = Subject.PhysicalEducation; break;
+
+        case "14": subject = Subject.German; break;
+
+        case "15": subject = Subject.Spanish; break;
+
+        case "16": subject = Subject.Philosophy; break;
+
+        case "17": subject = Subject.Informatics; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+
+    Console.Write("Enter remark type: ");
+    string type = Console.ReadLine()!;
+
+    RemarkType rmType = new RemarkType();
+
+    switch (type)
+    {
+        case "1": rmType = RemarkType.NoHomework; break;
+
+        case "2": rmType = RemarkType.DisruptiveBehavior; break;
+
+        case "3": rmType = RemarkType.LateSubmission; break;
+
+        case "4": rmType = RemarkType.ConcentrationIssues; break;
+
+        case "5": rmType = RemarkType.BadDiscipline; break;
+
+        case "6": rmType = RemarkType.SleepingInClass; break;
+
+        case "7": rmType = RemarkType.EatingInClass; break;
+
+        case "8": rmType = RemarkType.UnpreparedForClass; break;
+
+        case "9": rmType = RemarkType.RudeBehavior; break;
+
+        case "10": rmType = RemarkType.Other; break;
+
+        default: Console.WriteLine("Invalid option"); break;
+    }
+
+    Remark remark = new Remark(subject, rmType);
+
+    s.AddRemark(remark);
+
+    File.WriteAllText("data.json", JsonSerializer.Serialize(remark, options));
 }
 
 void DeleteStudent()
@@ -82,6 +345,18 @@ void StudentDetails()
 {
 }
 
+void AddTeacher()
+{
+    Console.Write("Teahcer first name: ");
+    var teacherFirst = Console.ReadLine() ?? "";
+
+    Console.Write("Teacher last name: ");
+    var teacherLast = Console.ReadLine() ?? "";
+
+    Teacher teacher = new Teacher(teacherFirst, teacherLast);
+
+    File.WriteAllText("data.json", JsonSerializer.Serialize(teacher, options));
+}
 
 void AddStudent(List<Student> list)
 {
@@ -149,9 +424,9 @@ void SaveData(List<Student> list)
 /*using System.Text.Json;
 using System.Text.Json.Nodes;
 
-JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+
 
 File.WriteAllText("data.json", "");
 string str = JsonSerializer.Serialize("black", options);
 */
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
